6a3025ff77624fd649255afda21ab4d1bb48962f
dideey <bradleyjd32@gmail.com>
2024-06-13 19:56:19 +0300

    app log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Flask App
 """
+import flask
 from flask import Flask, jsonify, request
 from auth import Auth
 
@@ -28,6 +29,21 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Post /sessions
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        return jsonify({"email": email,
+                        "message": "logged in",
+                        "session_id": session_id})
+    else:
+        return flask.abort(401)
 
 
 if __name__ == "__main__":
