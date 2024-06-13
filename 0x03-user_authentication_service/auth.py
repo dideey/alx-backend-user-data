21b50d96665d24b768ee12dc9b50e64eee06cc2e
dideey <bradleyjd32@gmail.com>
2024-06-13 18:49:58 +0300

    create session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,3 +52,14 @@ class Auth:
                 user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Creates a new session
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
