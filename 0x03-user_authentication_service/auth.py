cc429bff096b7707456d65b8d02414bd817897f6
dideey <bradleyjd32@gmail.com>
2024-06-13 20:12:43 +0300

    get user from session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -63,3 +63,11 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get the user from the session ID
+        """
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
